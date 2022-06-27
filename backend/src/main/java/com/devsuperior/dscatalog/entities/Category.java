0aa6be654f484031d23a652333501278c9de5f61
franklyn damaceno <frank-destro@outook.com>
2022-06-27 19:03:23 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,8 +3,19 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 
@@ -17,7 +28,7 @@ public class Category implements Serializable {
 	}
 
 	public Long getId() {
-		return id;
+		return id;	
 	}
 
 	public void setId(Long id) {
